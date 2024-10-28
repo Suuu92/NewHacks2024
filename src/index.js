fa7280f9b7d01c56341fd4017ad5b501e3f71c37
Suzen Bao <125599121+Suuu92@users.noreply.github.com>
2024-10-28 14:52:02 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,16 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import Earthquake from './pages/earthquake';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import EarthP1 from './earth/p1-1';
+import EarthP2 from './earth/p1-2';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/earthquake" element={<Earthquake />} />
+        <Route path="/p1-1" element={<EarthP1 />} />
+        <Route path="/p1-2" element={<EarthP2 />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
