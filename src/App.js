82d3e33173c3b6c605732dfc1777bd43fbbec87b
Suzen Bao <125599121+Suuu92@users.noreply.github.com>
2024-10-28 14:52:26 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function App() {
   return (
     <div>
       <div className="box">
-        <h2 style={{marginBottom: '20px'}}>Welcome to "Escape from Disaster"!</h2>
-          <p>In this game, you will be given different circumstances where something unknown might happen.</p>
-          <p>Your aim is to survive.</p>
-          <p>You have to make choices. </p>
-          <p>Your ending will depend on your choices,</p>
-          <p>so make sure to make wise choices. </p>
-        <h2 style={{marginTop: '20px', marginBottom: '20px'}}>Now, choose a theme to start your adventure!</h2>
-          
-          <button>Earthquake</button>
-          <button>Flood(TBD)</button>
-          <button >Hurricane(TBD)</button>
-        
+        <h1 style={{ marginBottom: '20px' }}>Welcome to "Escape from Disaster"!</h1>
+        <p style={{ fontSize: '20px' }}>In this game, you will face unexpected challenges at every turn.</p>
+        <p style={{ fontSize: '20px' }}>Your mission: make the right choices to endure and escape.</p>
+        <p style={{ fontSize: '20px' }}>Each decision changes your story, leading to different endings.</p>
+        <p style={{ fontSize: '20px' }}>Choose wisely- and may your instincts guide you to safety.</p>
+        <h2 style={{ marginTop: '20px', marginBottom: '50px' }}>Now, choose a theme to start your adventure!</h2>
+        <div style={{display: 'flex', flexDirection: 'row', justifyContent: 'center'}}>
+          <Link to="/earthquake" className='link'>Earthquake</Link>
+          <Link className='link'>Flood</Link>
+          <Link className='link'>Hurricane</Link>
+        </div>
       </div>
-    
     </div>
   );
 }
